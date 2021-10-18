051cf35ba63bebab7d5191fa7fc43a306500539f
Hugo Castillo <71911929+ungeimer@users.noreply.github.com>
2021-10-18 11:42:51 -0500

    first commit

---
+++ nearATM-api/src/main/java/com/academy/ibm/nearatm/NearATMApplication.java
@@ -0,0 +1,13 @@
+package com.academy.ibm.nearatm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearATMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearATMApplication.class, args);
+	}
+
+}
---
+++ nearATM-api/src/test/java/com/academy/ibm/nearatm/NearATMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.ibm.nearatm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearATMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
